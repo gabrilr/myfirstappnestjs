8d2e0670bbb675449d095a1f782b5df75b79e783
gabrilr <gabrilreyes30@gmail.com>
2026-09-22 19:54:20 -0600

    Mi primera app en nestjs2

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,8 +2,10 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller({})
 export class TaskController {
+
     @Get('/tasks')
     getAllTasks(){
         return 'This action returns all tasks';
     }
+    
 }
